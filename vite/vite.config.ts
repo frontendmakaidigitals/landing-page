d2d518d5a9044639c950bcc39006a86ae0e9f467
faheem221 <faheemk793@gmail.com>
2025-09-11 12:42:31 +0530

    added

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // Replace with your repo name
+  base: "landing-page",
 });
